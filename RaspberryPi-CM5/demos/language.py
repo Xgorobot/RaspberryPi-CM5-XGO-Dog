b0a662483f3f0458373c502d756beb1b85946f65
KEENNESS19 <147995398+KEENNESS19@users.noreply.github.com>
2025-04-18 16:28:23 +0800

    Update language.py
    After the update, it is no longer necessary to restart and has been adapted to self start

--- RaspberryPi-CM5/demos/language.py
+++ RaspberryPi-CM5/demos/language.py
@@ -1,6 +1,10 @@
+from numpy.f2py.crackfortran import previous_context
 from uiutils import (get_font, get_path, display_cjk_string, Button, color_white, color_bg,
-                   color_unselect, color_purple, draw, splash, display, lan_logo, arrow_logo_1, lal)
+                    color_unselect, color_purple, draw, splash, display, lan_logo, arrow_logo_1, lal)
+from uiutils import load_language
 import sys, time, os
+#import subprocess
+#import psutil  
 
 # Setup
 sys.path.append("..")
@@ -8,7 +12,6 @@ button = Button()
 
 font1 = get_font(16)
 font2 = get_font(18)
-
 
 language_ini_path = get_path("language_ini_path")
 
@@ -23,39 +26,59 @@ title_x = (320 - text_width) / 2
 display_cjk_string(draw, title_x, 90, lal["LANGUAGE"]["SET"], font_size=font2, color=color_white, background_color=color_bg)
 display.ShowImage(splash)
 
+def update_ui():
+    splash.paste(lan_logo, (133, 25), lan_logo)
+    text_width = draw.textlength(lal["LANGUAGE"]["SET"], font=font2)
+    title_x = (320 - text_width) / 2
+    display_cjk_string(draw, title_x, 90, lal["LANGUAGE"]["SET"], font_size=font2, color=color_white,
+                      background_color=color_bg)
 
-country_list = [["English", "en"], ["中文", "cn"]]
-select = 0
-
-while True:
-    # Draw UI
     draw.rectangle([(20, 145), (300, 180)], fill=color_purple)
     if content == "cn":
         draw.rectangle([(160, 146), (299, 179)], fill=color_unselect)
     else:
         draw.rectangle([(21, 146), (160, 179)], fill=color_unselect)
-    
-    # Display options
+
     display_cjk_string(draw, 80, 150, "CN", font_size=font2, color=color_white, background_color=color_bg)
     display_cjk_string(draw, 220, 150, "EN", font_size=font2, color=color_white, background_color=color_bg)
     splash.paste(arrow_logo_1, (148, 150), arrow_logo_1)
     display.ShowImage(splash)
 
-    # Button controls
-    if button.press_c(): content = "cn"     
-    elif button.press_d(): content = "en"   
-    elif button.press_a(): break             
-    elif button.press_b(): os._exit(0)       
 
-# Save settings
-with open(language_ini_path, "w") as f:
-    f.write(content)
+import os
 
-# Show confirmation
-text_width = draw.textlength(lal["LANGUAGE"]["SAVED"], font=font2)
-title_x = (320 - text_width) / 2
-display_cjk_string(draw, title_x, 200, lal["LANGUAGE"]["SAVED"], font_size=font2, color=color_white, background_color=color_bg)
-display.ShowImage(splash)
+def restart_application():
+    print(111)
+    with open(language_ini_path, "w") as f:
+        f.write(content)
+        f.flush()
 
-time.sleep(2)
-os.system('sudo reboot')  
+    print(222)
+    text_width = draw.textlength(lal["LANGUAGE"]["SAVED"], font=font2)
+    title_x = (320 - text_width) / 2
+    display_cjk_string(draw, title_x, 200, lal["LANGUAGE"]["SAVED"], font_size=font2, color=color_white, background_color=color_bg)
+    display.ShowImage(splash)
+    time.sleep(2)
+
+
+    os.system("python /home/pi/RaspberryPi-CM4/kill.py")
+    os._exit(0)  
+    
+while True:
+    previous_content = content
+    
+    update_ui()
+
+    if button.press_c(): 
+        content = "cn"
+    elif button.press_d(): 
+        content = "en"
+    elif button.press_b(): 
+        break  
+    elif button.press_a():
+        restart_application()  
+
+    if content != previous_content and button.press_a():
+        with open(language_ini_path, "w") as f:
+            f.write(content)
+            lal = load_language()
